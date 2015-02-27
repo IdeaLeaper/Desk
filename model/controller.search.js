4afd2a6e3a5b0806fddcc9aeeaaecf882335f76c
IdeaLeaper <idealeaper@asia.com>
2015-02-27 01:46:41 -0500

    fix bugs

--- model/controller.search.js
+++ model/controller.search.js
@@ -9,7 +9,7 @@ function(page, argv) {
 		var w = plus.nativeUI.showWaiting("正在获取百科...");
 		$.ajax({
 			type: 'GET',
-			url: 'http://' + localStorage.service + '/api/get_search_results/?include=custom_fields,attachments,title,excerpt&search=' + encodeURIComponent(argv.text) + "&page=" + p + "&china=" + localStorage.china,
+			url: 'http://' + localStorage.service + '/api/get_search_results/?include=custom_fields,title,excerpt&search=' + encodeURIComponent(argv.text) + "&page=" + p + "&china=" + localStorage.china,
 			dataType: 'json',
 			cache: false,
 			timeout: 20000,
@@ -24,11 +24,11 @@ function(page, argv) {
 						compound += i;
 					}
 					compound += ">";
-					if (data.posts[i]["custom_fields"].image[0]) {
+					if (data.posts[i]["custom_fields"].image) {
 						compound += "<div style='float: left;width:50px;height:50px;background:url(\"" + data.posts[i]["custom_fields"].image[0] + "?imageView2/1/w/50/h/50\");background-size:100% 100%;'></div><div style='margin-left:60px;margin-top:-1px;'>";
 					}
 					compound += "<b>" + without(data.posts[i].title);
-					if (data.posts[i]["custom_fields"].image[0]) {
+					if (data.posts[i]["custom_fields"].image) {
 						compound += "&nbsp;&nbsp;<i class='fa fa-picture-o'></i>";
 					}
 					var wexc = without(data.posts[i].excerpt) + "...";
